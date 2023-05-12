4a1203b77ce391484518b2fd449fda7692e2ef6a
Yury Landau <yury.landau@forpeople.io>
2023-05-12 18:00:59 -0300

    feat: more functions to test

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
 use ferris_says::say;
-use std::io::{stdout, BufWriter};
 
+// extern crate ferris_says;
+use std::{
+    fmt::Display,
+    io::{stdout, BufWriter},
+};
+/// This line goes to doc for main function
 fn main() {
+    //! This line goes to doc
+    //! Second doc line
+    println!("{} days", 31);
     let stdout = stdout();
     let message = String::from("Hello fellow Rustaceans!");
     let width = message.chars().count();
 
     let mut writer = BufWriter::new(stdout.lock());
     say(message.as_bytes(), width, &mut writer).unwrap();
+
+    Display!(make_string(927, "label"))
 }
-    
